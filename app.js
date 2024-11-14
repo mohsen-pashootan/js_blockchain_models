5bd593c308d86496a1ab7207a4c8854a1bb1487d
mohsen-pashootan <tahmasebpour.mohsen@gmail.com>
2024-11-14 16:39:48 +0330

    setup express for expose blocks throw api

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 
+
 const Block = require('./block')
 const Blockchain = require('./blockchain')
 const Transaction = require('./transaction')
 
-let transaction = new Transaction('Mary', 'John', 100)
+const express = require('express')
+const app = express() 
 
-let genesisBlock = new Block() 
-let blockchain = new Blockchain(genesisBlock)
+app.get('/blockchain', (req, res) => {
 
-let block = blockchain.getNextBlock([transaction])
-blockchain.addBlock(block)
+    let transaction = new Transaction('Mary', 'John', 100)
 
-let anotherTransaction = new Transaction('Steven', 'Brian', 500)
-let block1 = blockchain.getNextBlock([anotherTransaction, transaction])
-blockchain.addBlock(block1)
+    let genesisBlock = new Block() 
+    let blockchain = new Blockchain(genesisBlock)
 
-console.log(blockchain)
+    let block = blockchain.getNextBlock([transaction])
+    blockchain.addBlock(block)
+
+    let anotherTransaction = new Transaction('Steven', 'Brian', 500)
+    let block1 = blockchain.getNextBlock([anotherTransaction, transaction])
+    blockchain.addBlock(block1)
+
+    res.json(blockchain)
+
+})
+
+app.listen(8080, () => {
+    console.log('Server is running...')
+})
+
+
+
+
+
