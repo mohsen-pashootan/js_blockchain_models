9b7974332cc8d8b86ab3226893728556ad331811
mohsen-pashootan <tahmasebpour.mohsen@gmail.com>
2024-11-14 17:07:59 +0330

    add transaction route

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 
-
 const Block = require('./block')
 const Blockchain = require('./blockchain')
 const Transaction = require('./transaction')
 
 const express = require('express')
 const app = express() 
+
+// body parser for JSON
+app.use(express.json())
+
+let transactions = [] 
+
+app.post('/transactions', (req, res) => {
+
+    const to = req.body.to
+    const from = req.body.from 
+    const amount = req.body.amount 
+
+    let transaction = new Transaction(from, to, amount)
+    transactions.push(transaction)
+    res.json(transactions)
+})
 
 app.get('/blockchain', (req, res) => {
 
